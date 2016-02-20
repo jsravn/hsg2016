f786381a0cb921071b529e61d91315dc7ae12226
James Ravn <james@r-vn.org>
2016-02-20 19:22:43 +0000

    Homework 1 first 3 exercises

---
+++ HW01Tests.hs
@@ -0,0 +1,71 @@
+-- CIS 194, Spring 2015
+--
+-- Test cases for HW 01
+
+module HW01Tests where
+
+import           HW01
+import           Testing
+
+-- Exercise 1 -----------------------------------------
+
+testLastDigit :: (Integer, Integer) -> Bool
+testLastDigit (n, d) = lastDigit n == d
+
+testDropLastDigit :: (Integer, Integer) -> Bool
+testDropLastDigit (n, d) = dropLastDigit n == d
+
+ex1Tests :: [Test]
+ex1Tests = [ Test "lastDigit test" testLastDigit
+             [(123, 3), (1234, 4), (5, 5), (10, 0), (0, 0)]
+           , Test "dropLastDigit test" testDropLastDigit
+             [(123, 12), (1234, 123), (5, 0), (10, 1), (0,0)]
+           ]
+
+-- Exercise 2 -----------------------------------------
+
+testRevDigits :: (Integer, [Integer]) -> Bool
+testRevDigits (l, r) = toRevDigits l == r
+
+ex2Tests :: [Test]
+ex2Tests = [ Test "toRevDigits test" testRevDigits
+             [(9, [9]), (1234, [4, 3, 2, 1]), (0, []),
+              (421320, [0, 2, 3, 1, 2, 4])]
+           ]
+
+-- Exercise 3 -----------------------------------------
+
+testDoubleEveryOther :: ([Integer], [Integer]) -> Bool
+testDoubleEveryOther (l, r) = doubleEveryOther l == r
+
+ex3Tests :: [Test]
+ex3Tests = [ Test "doubleEveryOther test" testDoubleEveryOther
+             [([0], [0]), ([4, 9, 5, 5], [4, 18, 5, 10]),
+              ([2, 3, 5], [2, 6, 5])]
+           ]
+
+-- Exercise 4 -----------------------------------------
+
+ex4Tests :: [Test]
+ex4Tests = []
+
+-- Exercise 5 -----------------------------------------
+
+ex5Tests :: [Test]
+ex5Tests = []
+
+-- Exercise 6 -----------------------------------------
+
+ex6Tests :: [Test]
+ex6Tests = []
+
+-- All Tests ------------------------------------------
+
+allTests :: [Test]
+allTests = concat [ ex1Tests
+                  , ex2Tests
+                  , ex3Tests
+                  , ex4Tests
+                  , ex5Tests
+                  , ex6Tests
+                  ]
---
+++ lecture1.hs
@@ -0,0 +1,39 @@
+y :: Int
+y = y + 1
+biggestInt, smallestInt :: Int
+biggestInt  = maxBound
+smallestInt = minBound
+
+-- Booleans
+b1, b2 :: Bool
+b1 = True
+b2 = False
+
+-- Unicode characters
+c1, c2, c3 :: Char
+c1 = 'x'
+c2 = 'Ø'
+c3 = 'ダ'
+
+-- Strings are lists of characters with special syntax
+s :: String
+s = "Hello, Haskell!"
+
+ex01 = 3 + 2
+ex02 = 19 - 27
+ex03 = 2.35 * 8.6
+ex04 = 8.7 / 3.1
+ex05 = mod 19 3
+ex06 = 19 `mod` 3
+ex07 = 7 ^ 222
+ex08 = (-3) * (-7)
+
+
+hailstone :: Integer -> Integer
+hailstone n
+  | n `mod` 2 == 0 = n `div` 2
+  | otherwise      = 3*n + 1
+-- Generate the sequence of hailstone iterations from a starting number.
+hailstoneSeq :: Integer -> [Integer]
+hailstoneSeq 1 = [1]
+hailstoneSeq n = n : hailstoneSeq (hailstone n)
