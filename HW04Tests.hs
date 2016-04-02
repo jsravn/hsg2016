a880e4287306c9eb2505a29035c775052af402be
James Ravn <james@r-vn.org>
2016-04-02 21:01:17 +0100

    Remove redundant parentheses

--- HW04Tests.hs
+++ HW04Tests.hs
@@ -18,13 +18,13 @@ equalTests = TestList
 
 showTests :: Test
 showTests = TestList
-    [ TestCase (assertEqual "whole shebang" ("9x^4 + x^2 + x + -3") (show (P [-3, 1, 1, 0, 9] :: Poly Int)))
-    , TestCase (assertEqual "zero" ("0") (show (P [0, 0, 0, 0] :: Poly Int)))
-    , TestCase (assertEqual "one" ("x + 1") (show (P [1, 1] :: Poly Int)))
-    , TestCase (assertEqual "minus one" ("2x^2 + -x") (show (P [0, -1, 2] :: Poly Int)))
-    , TestCase (assertEqual "minus one again" ("x^2 + -1") (show (P [-1, 0, 1] :: Poly Int)))
-    , TestCase (assertEqual "x" ("x") (show (x :: Poly Int)))
-    , TestCase (assertEqual "leading zeros dropped" ("3x^3") (show (P [0, 0, 0, 3, 0, 0] :: Poly Int)))
+    [ TestCase (assertEqual "whole shebang" "9x^4 + x^2 + x + -3" (show (P [-3, 1, 1, 0, 9] :: Poly Int)))
+    , TestCase (assertEqual "zero" "0" (show (P [0, 0, 0, 0] :: Poly Int)))
+    , TestCase (assertEqual "one" "x + 1" (show (P [1, 1] :: Poly Int)))
+    , TestCase (assertEqual "minus one" "2x^2 + -x" (show (P [0, -1, 2] :: Poly Int)))
+    , TestCase (assertEqual "minus one again" "x^2 + -1" (show (P [-1, 0, 1] :: Poly Int)))
+    , TestCase (assertEqual "x" "x" (show (x :: Poly Int)))
+    , TestCase (assertEqual "leading zeros dropped" "3x^3" (show (P [0, 0, 0, 3, 0, 0] :: Poly Int)))
     ]
 --
 
